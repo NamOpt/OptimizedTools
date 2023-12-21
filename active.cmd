d039a02a2dbf88e3240178b3a41e58b4bb018d26
NamGitHub2002 <120299835+NamGitHub2002@users.noreply.github.com>
2023-12-21 05:47:41 -0800

    Add files via upload

---
+++ active.cmd
@@ -0,0 +1,48 @@
+@echo off
+mode 76, 30
+title All-in-one Tools - menu 1
+cls
+echo:
+echo:                    [31mAll[0m - [33min[0m - [32mone[0m [36mTool[0m [beta]
+echo:             __________________________________________________      
+echo:
+echo:                 Microsoft Activation Scripts:
+echo:
+echo:             [1] HWID             ^|  Windows           ^|   Permanent
+echo:             [2] Ohook            ^|  Office            ^|   Permanent
+echo:             [3] KMS38            ^|  Windows           ^|   Year 2038
+echo:             [4] Online KMS       ^|  Windows / Office  ^|    180 Days
+echo:             [5] Exit
+echo:             __________________________________________________   
+echo:
+echo:          [46mEnter a menu option in the Keyboard [1,2,3,4,5] :[0m
+choice /C:12345 /N
+set _erl=%errorlevel%
+
+
+if %_erl%==5 goto 5
+if %_erl%==4 goto 4
+if %_erl%==3 goto 3
+if %_erl%==2 goto 2
+if %_erl%==1 goto 1
+
+:1
+cd Active
+1.cmd
+cd..
+:2
+cd Active
+2.cmd
+cd..
+:3
+cd Active
+3.cmd
+cd..
+:4
+cd Active
+4.cmd
+cd..
+:5
+cd..
+cd Main
+main.cmd
