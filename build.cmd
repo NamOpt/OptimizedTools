3a6f10b24d6ed3de3354081909d13cb049274e22
Nam <nam3244@outlook.com>
2024-01-30 18:40:16 +0700

    Add files via upload

---
+++ build.cmd
@@ -0,0 +1,19 @@
+@echo off
+title OptimizedTools build tools [0.1]
+echo OptimizedTools build tools [0.1]
+echo Welcome!
+echo Press [Enter] to build OptimizedTools ver 0.2.2.1
+pause >nul
+echo Please wait
+ping 127.0.0.1 -n 3 > nul
+echo Building
+echo This take a while please wait
+ping 127.0.0.1 -n 15 > nul
+echo Complete in %time% %date%
+ping 127.0.0.1 -n 2 > nul
+echo Open Windows default editor please wait
+ping 127.0.0.1 -n 2 > nul
+cd Main
+notepad main.cmd
+echo bye :3
+ping 127.0.0.1 -n 2 > nul
